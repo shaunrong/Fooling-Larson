e45c8b7e66ae4547b253711073c633dd38fd29c7
rongzq <rongzq08@mit.edu>
2015-05-10 14:09:12 -0400

    put up a experiment module

--- exp/__init__.py
+++ exp/__init__.py
@@ -4,4 +4,3 @@ __author__ = 'Shaun Rong'
 __version__ = '0.1'
 __maintainer__ = 'Shaun Rong'
 __email__ = 'rongzq08@gmail.com'
-
---
+++ exp/experiment_1.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+from Clustering import UPGMA
+
+__author__ = 'Shaun Rong'
+__version__ = '0.1'
+__maintainer__ = 'Shaun Rong'
+__email__ = 'rongzq08@gmail.com'
+
+from larson.InputWorld import Digits
+from larson.GSOM import GSOM
+
+
+def main():
+    input_world = Digits(1, norm='../norms/digits.yaml')
+    SOM = GSOM(alpha=0.1, lam=10, tou=0.1)
+    #debug
+    it = 0
+    while not SOM.converged:
+        SOM.update(input_world.ran_input())
+        print it
+        it += 1
+    clusters = UPGMA(SOM)
+    clusters.clustering()
+    print clusters.representative
+    print clusters.association
+
+if __name__ == '__main__':
+    main()
