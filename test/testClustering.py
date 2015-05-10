c8625487c974c878a1a60504d62f8ff74a3fc80c
rongzq <rongzq08@mit.edu>
2015-05-10 16:50:24 -0400

    add test file folder

---
+++ test/testClustering.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+__author__ = 'Shaun Rong'
+__version__ = '0.1'
+__maintainer__ = 'Shaun Rong'
+__email__ = 'rongzq08@gmail.com'
+
+import unittest
+
+class testClustering(unittest.TestCase):
